9d8d758fe994da0446555a52018d12ac037ba4e8
stangatimu <stangatimu@gmail.com>
2026-01-23 14:50:43 +0300

    ch: testing if a change will trigger a fresh deployement

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello World! From DigitalOcean</p>
+        <p>Testing autodeploy from GitHub Actions</p>
         <a
           className="App-link"
           href="https://reactjs.org"
